7db8a53cea8f5eb32ffb9a172c0f7e9ca1454c7d
Adrien Castelbou <adrien@42berlin.de>
2022-09-25 10:52:51 +0200

    get datas from fetching

--- src/components/Cam.jsx
+++ src/components/Cam.jsx
@@ -6,13 +6,22 @@ import { useRef } from 'react';
 export default function Cam(){
     const webRef = useRef(null);
     var ImageRaw = null;
-    var Example = null;
-    var TransformRawTojpeg = ({ ImageRaw }) => <img src={`data:image/jpeg;base64,${ImageRaw}`} /> 
     const takePicture= () => {
         ImageRaw = webRef.current.getScreenshot();
-        Example = ({ ImageRaw }) => <img src={`data:image/jpeg;base64,${ImageRaw}`} />
-
         console.log(ImageRaw)
+    }
+
+    const analyze= async () => {
+        const response = await fetch("http://127.0.1:5000/detect_items", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: ImageRaw
+        });
+        const content = await response.json();
+        console.log(content);
     }
 
     return (
@@ -20,7 +29,10 @@ export default function Cam(){
          <Webcam ref={webRef}/>
          <button onClick={()=>{
             takePicture();
-         }}>Send Image</button>
+         }}>Take Picture</button>
+         <button onClick={()=>{
+            analyze();
+         }}>Analyze</button>
         </div>
     )
 }
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect, Component } from 'react'
 import { HiSearch } from "react-icons/hi"
 
 const Searchbar = () => {
+    const [data, setData] = useState('');
+    
+
+    const SearchItem = async () =>  {
+        const response = await fetch("http://127.0.1:5000/items", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({data: [data]})
+        });
+        const content = await response.json();
+        console.log(content);
+        setData("")
+    }
+
     return (
         <section>
               <div class="search-container">
-                <form action="/action_page.php">
-                <input type="text" placeholder="Search.." name="search"/>
-                <button type="submit" className = "container-button"><HiSearch/></button>
-                </form>
+                <input type="text" placeholder="Search.." name="data" onChange={(e) => setData(e.target.value)}/>
+                <button className = "container-button" onClick={() => SearchItem()}><HiSearch/></button>
              </div>
         </section>
     )
